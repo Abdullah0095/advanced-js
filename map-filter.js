1009a27334b2c3ffcbbbb19bbe354a05c8f00669
unknown <abdullah.web1995@gmail.com>
2020-08-07 12:25:27 +0600

    map filter work

---
+++ map-filter.js
@@ -0,0 +1,30 @@
+// const numbers = [4, 5, 6, 7, 8];
+// const output = [];
+
+// for(let i = 0; i < numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+
+// }
+
+
+// function square(element){
+//     return element * element;
+// }
+
+// const square = x => x * x;
+
+// const numbers = [4, 5, 6, 7, 8];
+// const result = numbers.map(x => x * x)
+// console.log(result);
+
+// numbers.map(function(element, array){
+//     console.log(element, array);
+// })
+
+// console.log(output);
+
+const numbers = [4, 5, 6, 7, 8];
+const bigger = numbers.filter(x => x<4);
+console.log(bigger)
