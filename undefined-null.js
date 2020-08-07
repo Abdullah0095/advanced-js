e4bf2173482a26cb2f9abd3424feaaea238b88d8
unknown <abdullah.web1995@gmail.com>
2020-08-07 11:45:21 +0600

    null vs undefined interview

---
+++ undefined-null.js
@@ -0,0 +1,15 @@
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num2, num1)
+}
+
+
+const person = {name: "samrat", phone: 01919};
+console.log(person.gf)
+
+let fun = undefined;
+console.log(fun)
